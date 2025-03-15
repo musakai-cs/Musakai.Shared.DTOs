2abafca83aafc0b277f849338f38a246cde84365
Daniel Klement <info@danielklement.net>
2025-03-15 01:25:00 +0100

    Add project files.

---
+++ Musakai.Shared.DTOs/Requests/EmailService/ContactEmailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Musakai.Shared.DTOs.Requests.EmailService;
+
+public sealed class ContactEmailRequest
+{
+    public string Receiver { get; set; } = string.Empty;
+    public string Subject { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+    public string Sender { get; set; } = string.Empty;
+}
